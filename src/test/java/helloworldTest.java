575908afa05f1a1abb840bb62f6fa7cb035aa19a
Jiaojiao Chen <beams_boy@163.com>
2016-08-30 16:28:48 -0400

    Add layout,Junit4,first test case

---
+++ src/test/java/helloworldTest.java
@@ -0,0 +1,16 @@
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+
+public class helloworldTest {
+	@Before
+	public void setUp() {
+	}
+
+	@Test
+	public void testhelloworld() {
+	assertEquals(true,true);	
+	}
+
+}
