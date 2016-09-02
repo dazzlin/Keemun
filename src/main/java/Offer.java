b9abd43023c2761698e04aff79d6a5d213115ac2
Jiaojiao Chen <beams_boy@163.com>
2016-09-02 12:38:48 -0400

    Add Offer, Wage classes and fail Unit Tests

---
+++ src/main/java/Offer.java
@@ -0,0 +1,16 @@
+
+public class Offer {
+  private Wage wage;
+  
+  public Offer(Wage wage) {
+    this.wage = new Wage();
+  }
+  
+  public int getWage() {
+    return this.wage;
+  }
+  
+  public void setPrice(int price) {
+    this.price = price;
+  }
+}
